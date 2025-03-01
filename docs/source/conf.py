61362757ecc419f4c4e14d707050144a0938ce22
Pluglug <upward571103@gmail.com>
2025-03-02 02:23:32 +0900

    Formatting Sphinx configuration files with Black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,38 +6,42 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Modular Renamer'
-copyright = '2025, Pluglug'
-author = 'Pluglug'
-release = '0.1'
+project = "Modular Renamer"
+copyright = "2025, Pluglug"
+author = "Pluglug"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',  # Google/Numpy形式のDocstring対応
-    'sphinx.ext.viewcode',  # ソースコード表示
-    'sphinx.ext.intersphinx',  # 外部ドキュメントへのリンク
-    'sphinx.ext.inheritance_diagram',  # 継承関係の図
-    'sphinx.ext.graphviz', # クラス図
-    'sphinx_automodapi.automodapi', # モジュールのAPIドキュメント
-    'sphinxcontrib.mermaid', # マーメイド図
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # Google/Numpy形式のDocstring対応
+    "sphinx.ext.viewcode",  # ソースコード表示
+    "sphinx.ext.intersphinx",  # 外部ドキュメントへのリンク
+    "sphinx.ext.inheritance_diagram",  # 継承関係の図
+    "sphinx.ext.graphviz",  # クラス図
+    "sphinx_automodapi.automodapi",  # モジュールのAPIドキュメント
+    "sphinxcontrib.mermaid",  # マーメイド図
     "myst_parser",
     "sphinx_design",
     "sphinx_copybutton",
 ]
 
-autodoc_mock_imports = ['bpy', 'mathutils']
+autodoc_mock_imports = ["bpy", "mathutils"]
 
 inheritance_graph_attrs = dict(rankdir="TB", size='"6.0, 8.0"')
-inheritance_node_attrs = dict(shape='rect', fontsize=12, height=0.4,
-                             margin='"0.08, 0.03"')
+inheritance_node_attrs = dict(
+    shape="rect", fontsize=12, height=0.4, margin='"0.08, 0.03"'
+)
 
 mermaid_params = [
-    '--theme', 'default',
-    '--width', '100%',
-    '--backgroundColor', 'transparent'
+    "--theme",
+    "default",
+    "--width",
+    "100%",
+    "--backgroundColor",
+    "transparent",
 ]
 
 mermaid_version = "11.2.0"  # 使用するMermaidのバージョン
@@ -65,12 +69,12 @@ mermaid.initialize({
 #     '.md': 'markdown',
 # }
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_title = 'Modular Renamer (Dev)'
-html_static_path = ['_static']
+html_theme = "furo"
+html_title = "Modular Renamer (Dev)"
+html_static_path = ["_static"]
